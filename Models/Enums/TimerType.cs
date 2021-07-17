1584a6c3ae6a8999c1e89e556ace8f8cc8b256e7
José Ricardo David <david_Ricardo@live.com>
2021-07-17 15:47:11 -0300

    Add project files.

---
+++ Models/Enums/TimerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PomodoroApp.Models.Enums
+{
+    public enum TimerType
+    {
+        Pomodoro = 1,
+        ShortBreak = 2,
+        LongBreak = 3
+    }
+}
---
+++ Models/PomodoroSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PomodoroApp.Models
+{
+    public class PomodoroSettings
+    {
+        [Required(ErrorMessage ="Specify the Pomodoro duration time.")]
+        [Range(1, 60, ErrorMessage ="Pomodoro duration time invalid. The value must be between 1 and 60.")]
+        public int PomodoroDuration { get; set; }
+
+        [Range(1,15 , ErrorMessage ="Short break time invalid. The value must be between 1 and 15.")]
+        public int ShortBreak { get; set; }
+
+        [Range(15, 60, ErrorMessage ="Short break time invalid. The value must be between 15 and 60.")]
+        public int LongBreak { get; set; }
+    }
+}
